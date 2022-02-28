efaf1875c3ba7dc633ad9c5d234b9e684773d7b2
SoraxZed <100567336+SoraxZed@users.noreply.github.com>
2022-02-28 17:30:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnYes: 'Em Yêu Anh <33',
+    btnNo: 'Không được đâu :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 10h, anh phệt em nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Anh biết mà. Yêu em nhiều nhiều <3',
+    messDesc: 'Tối nay , anh phệt em nha.',
+    btnAccept: 'Được luôn anh ưiiii <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100008395813019' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
